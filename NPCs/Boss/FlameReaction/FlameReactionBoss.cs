84e4caa2568d64e1f5b6fde10d97c00749c50235
OverwhelmingBlink <1304493352@qq.com>
2022-10-13 18:29:35 +0800

    重置了焰色反应的视觉效果
    现在有：拉丝拖尾
    丝滑.jpg

--- NPCs/Boss/FlameReaction/FlameReactionBoss.cs
+++ NPCs/Boss/FlameReaction/FlameReactionBoss.cs
@@ -22,9 +22,10 @@ namespace HeroRegression.NPCs.Boss.FlameReaction
             DisplayName.AddTranslation((int)GameCulture.CultureName.Chinese, "焰色反应");
             NPCID.Sets.TrailCacheLength[NPC.type] = 24;
             NPCID.Sets.TrailingMode[NPC.type] = 3;
-        
+
         }
-        public override bool CanHitPlayer(Player target, ref int cooldownSlot) { 
+        public override bool CanHitPlayer(Player target, ref int cooldownSlot)
+        {
             return Vector2.Distance(target.Center, NPC.Center) <= 25f;
         }
         public override void SetDefaults()
@@ -32,7 +33,7 @@ namespace HeroRegression.NPCs.Boss.FlameReaction
             NPC.width = 100;
             NPC.height = 100;
             NPC.friendly = false;
-            NPC.damage = Main.expertMode ?16 : 20;
+            NPC.damage = Main.expertMode ? 16 : 20;
             NPC.defense = 10;
             NPC.lifeMax = Main.expertMode ? 1500 : 2000;
             NPC.boss = true;
@@ -47,13 +48,67 @@ namespace HeroRegression.NPCs.Boss.FlameReaction
         {
             NPC.lifeMax = (int)(NPC.lifeMax * .66666666666666667f * bossLifeScale);
         }
-   
-
+        public float DiscoFunction(float currentTime, float cycleTime)
+        {
+            float partTime = currentTime % cycleTime;
+            if (partTime <= cycleTime / 3)
+            {
+                float value = (float)Math.Min(partTime / cycleTime * 3f, 1f);
+                return value;
+            }
+            else
+            {
+                if (partTime > cycleTime * 2f / 3)
+                {
+                    return 0;
+                }
+                else
+                {
+                    float value = (float)Math.Min(partTime / cycleTime * -3f + 2f, 1f);
+                    return value;
+                }
+            }
+        }
+        public Color TimeToDiscoColor(int currentTime, int cycleTime)
+        {
+            float r = DiscoFunction(currentTime + (int)(cycleTime * 2f / 3f), cycleTime);
+            float g = DiscoFunction(currentTime, cycleTime);
+            float b = DiscoFunction(currentTime + (int)(cycleTime / 3f), cycleTime);
+            return new Color(r, g, b);
+        }
         public override bool PreDraw(SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor)
         {
             Texture2D tex = GetTex("HeroRegression/Textures/FlameReactionTex");
             Vector2 ori = new Vector2(32, 32);
             Vector2 pos1 = NPC.Center - Main.screenPosition;
+            if (GlobalTimer > 25)
+            {
+                for (int j = 0; j <= 300; j += 60)
+                {
+                    List<VertexInfo2> vertices = new();
+                    for (int i = 0; i <= 23; i += 1)
+                    {
+                        if (NPC.oldPos[i] != Vector2.Zero)
+                        {
+                            Vector2 vertex0 = NPC.oldPos[i] + NPC.Size / 2 - Main.screenPosition;
+                            Vector2 vertex1 = vertex0 + MathHelper.ToRadians(GlobalTimer + j + 60).ToRotationVector2() * Math.Min((GlobalTimer - i) * 4, 120);
+                            Vector2 vertex2 = vertex0 + MathHelper.ToRadians(GlobalTimer + j - 60).ToRotationVector2() * Math.Min((GlobalTimer - i) * 4, 120);
+                            Color color = P2 ? TimeToDiscoColor((int)GlobalTimer - i, 300) : Color.White;
+                            vertices.Add(new VertexInfo2(vertex1, new Vector3((float)i / 24, 0, (float)i / 24), color));
+                            vertices.Add(new VertexInfo2(vertex2, new Vector3((float)i / 24, 1, (float)i / 24), color));
+                        }
+                    }
+                    spriteBatch.End();
+                    spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.Additive, SamplerState.AnisotropicClamp, DepthStencilState.None, RasterizerState.CullNone, null, Main.GameViewMatrix.TransformationMatrix);
+                    Main.graphics.GraphicsDevice.Textures[0] = GetTex("HeroRegression/Extra/BladeTrailDouble");
+                    if (vertices.Count >= 3)
+                    {
+                        Main.graphics.GraphicsDevice.DrawUserPrimitives(PrimitiveType.TriangleStrip, vertices.ToArray(), 0, vertices.Count - 2);
+                    }
+                    spriteBatch.End();
+                    spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.AnisotropicClamp, DepthStencilState.None, RasterizerState.CullNone, null, Main.GameViewMatrix.TransformationMatrix);
+                }
+            }
             if (HasTrail)
             {
                 for (int i = 0; i <= 23; i += 1)
@@ -65,7 +120,7 @@ namespace HeroRegression.NPCs.Boss.FlameReaction
                     }
                     if (P2)
                     {
-                        drawColor = (Dash ? Color.White : Main.DiscoColor) * ((float)(NPC.oldPos.Length - i) / (NPC.oldPos.Length));
+                        drawColor = (Dash ? Color.White : TimeToDiscoColor((int)GlobalTimer - i, 300)) * ((float)(NPC.oldPos.Length - i) / (NPC.oldPos.Length));
                     }
                     spriteBatch.End();
                     spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.Additive, SamplerState.AnisotropicClamp, DepthStencilState.None, RasterizerState.CullNone, null, Main.GameViewMatrix.TransformationMatrix);
@@ -129,26 +184,6 @@ namespace HeroRegression.NPCs.Boss.FlameReaction
             }
             SpellTimer++;
             GlobalTimer++;
-            if (GlobalTimer % 1 == 0)
-            {
-                float dustD = Math.Min(GlobalTimer / 2f, 60);
-                float dustR1 = MathHelper.ToRadians(GlobalTimer);
-                for (int i = 0; i <= 300; i += 60)
-                {
-                    float dustR2 = dustR1 + MathHelper.ToRadians(i);
-                    for (int j = -60; j <= 60; j += 4)
-                    {
-                        float dustR3 = dustR2 + MathHelper.ToRadians(j);
-                        Vector2 dustPos = dustR3.ToRotationVector2() * dustD / (float)Math.Cos(MathHelper.ToRadians(j));
-                        Dust dust = Dust.NewDustDirect(dustPos + NPC.Center, 1, 1, DustID.WhiteTorch, 0, 0, 0);
-                        dust.color = P2 ? Main.DiscoColor : Color.White;
-                        dust.velocity = NPC.velocity;
-                        dust.noGravity = true;
-                        dust.scale = .9f;
-                        dust.fadeIn = .6f;
-                    }
-                }
-            }
             switch (State)
             {
                 case NState.P1Dash1:
@@ -174,8 +209,8 @@ namespace HeroRegression.NPCs.Boss.FlameReaction
                     {
                         if (SpellTimer == 1)
                         {
-                            
-                             SoundEngine.PlaySound(SoundID.Roar, NPC.Center);
+
+                            SoundEngine.PlaySound(SoundID.Roar, NPC.Center);
                             DashPos = Main.LocalPlayer.Center + Vector2.Normalize(-NPC.Center + Main.LocalPlayer.Center) * (Vector2.Distance(NPC.Center, Main.LocalPlayer.Center) + 500);
                         }
                         if (SpellTimer > 1)
@@ -201,7 +236,7 @@ namespace HeroRegression.NPCs.Boss.FlameReaction
                         if (SpellTimer % 15 == 0)
                         {
                             SoundEngine.PlaySound(SoundID.Item9, NPC.Center);
-                            Projectile.NewProjectile(null,Main.LocalPlayer.Center + new Vector2(Main.rand.NextFloat(-480, 480), -800), new Vector2(0, 3), ModContent.ProjectileType<FlameReactionStar>(), 5, .1f, Main.myPlayer);
+                            Projectile.NewProjectile(null, Main.LocalPlayer.Center + new Vector2(Main.rand.NextFloat(-480, 480), -800), new Vector2(0, 3), ModContent.ProjectileType<FlameReactionStar>(), 5, .1f, Main.myPlayer);
                         }
                         if (SpellTimer > 300)
                         {
@@ -257,21 +292,21 @@ namespace HeroRegression.NPCs.Boss.FlameReaction
                             {
                                 for (int i = 1; i <= 3; i++)
                                 {
-                                    Projectile.NewProjectile(null,NPC.Center, Vector2.Zero, ModContent.ProjectileType<FlameReactionBulletP2>(), 5, .1f, Main.myPlayer, i);
+                                    Projectile.NewProjectile(null, NPC.Center, Vector2.Zero, ModContent.ProjectileType<FlameReactionBulletP2>(), 5, .1f, Main.myPlayer, i);
                                 }
                             }
                             if (NPC.life < NPC.lifeMax * .5f)
                             {
                                 for (int i = 1; i <= 4; i++)
                                 {
-                                    Projectile.NewProjectile(null,NPC.Center, Vector2.Zero, ModContent.ProjectileType<FlameReactionBulletP2>(), 5, .1f, Main.myPlayer, i);
+                                    Projectile.NewProjectile(null, NPC.Center, Vector2.Zero, ModContent.ProjectileType<FlameReactionBulletP2>(), 5, .1f, Main.myPlayer, i);
                                 }
                             }
                         }
-                        if (SpellTimer >1)
+                        if (SpellTimer > 1)
                         {
                             float Rot = (NPC.Center - Main.LocalPlayer.Center).ToRotation();
-                            if (Math.Abs(Main.LocalPlayer.Center.X - NPC.Center.X)>= 32f)
+                            if (Math.Abs(Main.LocalPlayer.Center.X - NPC.Center.X) >= 32f)
                             {
                                 Rot += Main.LocalPlayer.Center.X <= NPC.Center.X ? -MathHelper.ToRadians(.2f * (float)Math.Log(SpellTimer / 3f, Math.E)) : MathHelper.ToRadians(.2f * (float)Math.Log(SpellTimer / 3f, Math.E));
                                 Vector2 LerpPos = Main.LocalPlayer.Center + Rot.ToRotationVector2() * 500;
@@ -306,8 +341,6 @@ namespace HeroRegression.NPCs.Boss.FlameReaction
                             NPC.velocity = (DashPos - NPC.Center) / 30;
                             if (SpellTimer == 90)
                             {
-                                SoundEngine.PlaySound(SoundID.Roar, NPC.Center);
-                                Projectile.NewProjectile(null,Main.LocalPlayer.Center + new Vector2(0, -150), Vector2.Zero, ModContent.ProjectileType<FallingSlash>(), 11, .1f);
                                 SpellTimer = 0;
                                 Dash = false;
                                 SwitchTo(NState.P2Teleport);
@@ -333,7 +366,7 @@ namespace HeroRegression.NPCs.Boss.FlameReaction
                             {
                                 SoundEngine.PlaySound(SoundID.Item9, NPC.Center);
                                 Vector2 shoot1 = Vector2.Normalize(Main.LocalPlayer.Center - NPC.Center) * 15f;
-                                Projectile.NewProjectile(null,NPC.Center, shoot1, ModContent.ProjectileType<FlameReactionBulletP2>(), 5, .1f, Main.myPlayer, 5);
+                                Projectile.NewProjectile(null, NPC.Center, shoot1, ModContent.ProjectileType<FlameReactionBulletP2>(), 5, .1f, Main.myPlayer, 5);
                             }
                         }
                         if (NPC.life < NPC.lifeMax * .5f)
@@ -342,7 +375,7 @@ namespace HeroRegression.NPCs.Boss.FlameReaction
                             {
                                 SoundEngine.PlaySound(SoundID.Item9, NPC.Center);
                                 Vector2 shoot1 = Vector2.Normalize(Main.LocalPlayer.Center - NPC.Center) * 15f;
-                                Projectile.NewProjectile(null,NPC.Center, shoot1, ModContent.ProjectileType<FlameReactionBulletP2>(), 5, .1f, Main.myPlayer, 5);
+                                Projectile.NewProjectile(null, NPC.Center, shoot1, ModContent.ProjectileType<FlameReactionBulletP2>(), 5, .1f, Main.myPlayer, 5);
                             }
                         }
                         if (SpellTimer == 360)
